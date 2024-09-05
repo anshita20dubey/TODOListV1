f5b64c2dde18078eb081f7717cbee85b570dcfc2
anshita20dubey <anshita20dubey@gmail.com>
2024-09-05 15:01:10 +0530

    Version 1 of TODOLIST

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AddTodo = () => {
+    return (
+        <div className='container'>
+            <div class="row kg-row">
+                <div class="col-6">
+                    <input type="text" placeholder='Enter todo here' />
+                </div>
+                <div class="col-4">
+                    <input type="date" />
+                </div>
+                <div class="col-2">
+                    <button type="button" class="btn kg-button btn-success">Add</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AddTodo
